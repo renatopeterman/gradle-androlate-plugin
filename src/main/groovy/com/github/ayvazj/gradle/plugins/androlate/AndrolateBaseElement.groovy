28d2d5b013968f102d1b081a6d3c704e4c3743da
Renato Peterman <renato.pet@gmail.com>
2016-06-15 20:58:43 -0300

    get only field with translatable == false

--- src/main/groovy/com/github/ayvazj/gradle/plugins/androlate/AndrolateBaseElement.groovy
+++ src/main/groovy/com/github/ayvazj/gradle/plugins/androlate/AndrolateBaseElement.groovy
@@ -66,7 +66,7 @@ abstract class AndrolateBaseElement {
      * @return
      */
     def public void updateDestXml(Element destxml, List<TranslationsResource> translatedResources) {
-        if ("string".equals(this.node.getNodeName())) {
+        if ("string".equals(this.node.getNodeName()) && (this.node.hasAttribute('translatable') && this.node.getAttribute('translatable') != 'false')) {
             def string_name = this.node.getAttribute('name')
             def existing = null
             def string_elems = destxml.getElementsByTagName('string')
